0f6270b3dcd2639e84b624b8d2d7f2052264af8e
Nazar868 <zagariynazar04@gmail.com>
2026-06-19 15:11:54 +0300

    Create HibernateUtil.java

---
+++ src/main/java/org/example/HibernateUtil.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory =
+            new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+
+    public static SessionFactory getInstance() {
+        return sessionFactory;
+    }
+}
